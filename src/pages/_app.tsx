ab438dacdfe6abe30920dcbe863e5705327daf05
Eros0104 <erosmendonca@outlook.com>
2021-09-14 21:00:36 -0300

    feat: ThemeProvider do MaterialUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import MUITheme from 'src/styles/materialTheme';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <ThemeProvider theme={MUITheme}>
+    <Component {...pageProps} />;
+  </ThemeProvider>
+);
 export default MyApp;
---
+++ src/styles/materialTheme/index.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const MUITheme = createTheme({
+  palette: {
+    primary: {
+      main: '#CCA43B',
+    },
+  },
+});
+
+export default MUITheme;
