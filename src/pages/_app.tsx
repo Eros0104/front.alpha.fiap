a7dbae7b54e72001fb8d6a8ba90919841fa26e12
Eros0104 <erosmendonca@outlook.com>
2021-10-05 22:36:44 -0300

    fix: correção prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import GlobalStyles from 'src/styles/global';
 import type { AppProps } from 'next/app';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import { makeStyles } from '@material-ui/core/styles';
 import {
